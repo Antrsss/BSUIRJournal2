23b59103ada71d880d56e653183347294a1a73cb
Darya Zgirskaya <129535397+Antrsss@users.noreply.github.com>
2024-06-05 20:29:19 +0300

    final (fixed)

--- app/src/main/java/com/example/bsuirjournal2/viewmodels/NotesViewModel.kt
+++ app/src/main/java/com/example/bsuirjournal2/viewmodels/NotesViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.bsuirjournal2.viewmodels
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -8,6 +11,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.bsuirjournal2.GroupNumbersApplication
 import com.example.bsuirjournal2.data.NotesRepository
+import com.example.bsuirjournal2.model.Group
 import com.example.bsuirjournal2.model.Note
 import com.example.bsuirjournal2.model.User
 import kotlinx.coroutines.CoroutineScope
@@ -15,20 +19,43 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
+import java.io.IOException
 
+
+sealed interface NotesUiState {
+    data class Success(
+        val notes: List<Note>,
+    ) : NotesUiState
+    object Error : NotesUiState
+    object Loading : NotesUiState
+}
 class NotesViewModel(
     private val notesRepository: NotesRepository
 ) : ViewModel() {
 
-    var notes = mutableListOf<Note>()
+    var notesUiState: NotesUiState by mutableStateOf(NotesUiState.Loading)
+        private set
 
     fun postANote(token: String?, noteToPost: Note) {
         notesRepository.postANote("Bearer $token", noteToPost).enqueue(object : Callback<Note> {
             override fun onResponse(call: Call<Note>, response: Response<Note>) {
+                viewModelScope.launch {
+                    notesUiState = try {
+                        NotesUiState.Success(
+                            notesRepository.getAllNotes("Bearer $token")
+                        )
+                    } catch (e: IOException) {
+                        NotesUiState.Error
+                    } catch (e: HttpException) {
+                        NotesUiState.Error
+                    }
+                }
                 Log.d("FUCK YEAH", "Posted")
             }
             override fun onFailure(call: Call<Note>, t: Throwable) {
+                notesUiState = NotesUiState.Error
                 Log.d("FUCK NO", "not Posted")
             }
         })
@@ -37,9 +64,21 @@ class NotesViewModel(
 
         notesRepository.patchANote("Bearer $token", id, noteToPatch).enqueue(object : Callback<Note> {
             override fun onResponse(call: Call<Note>, response: Response<Note>) {
+                viewModelScope.launch {
+                    notesUiState = try {
+                        NotesUiState.Success(
+                            notesRepository.getAllNotes("Bearer $token")
+                        )
+                    } catch (e: IOException) {
+                        NotesUiState.Error
+                    } catch (e: HttpException) {
+                        NotesUiState.Error
+                    }
+                }
                 Log.d("FUCK YEAH", "Patched")
             }
             override fun onFailure(call: Call<Note>, t: Throwable) {
+                notesUiState = NotesUiState.Error
                 Log.d("FUCK NO", "not Patched")
             }
         })
@@ -49,12 +88,24 @@ class NotesViewModel(
 
         notesRepository.deleteANote("Bearer $token", id).enqueue(object : Callback<Note> {
             override fun onFailure(call: Call<Note>, t: Throwable) {
+                notesUiState = NotesUiState.Error
                 Log.d("del", "del failed")
             }
             override fun onResponse(
                 call: Call<Note>,
                 response: Response<Note>
             ) {
+                viewModelScope.launch {
+                    notesUiState = try {
+                        NotesUiState.Success(
+                            notesRepository.getAllNotes("Bearer $token")
+                        )
+                    } catch (e: IOException) {
+                        NotesUiState.Error
+                    } catch (e: HttpException) {
+                        NotesUiState.Error
+                    }
+                }
                 Log.d("del", "del not failed")
             }
         })
@@ -72,10 +123,19 @@ class NotesViewModel(
         })
     }
 
+
     fun getAllNotes(token: String?) {
         viewModelScope.launch {
-            val myNotes = notesRepository.getAllNotes("Bearer $token").toMutableList()
-            notes = myNotes.toMutableList()
+            notesUiState = NotesUiState.Loading
+            notesUiState = try {
+                NotesUiState.Success(
+                    notesRepository.getAllNotes("Bearer $token")
+                )
+            } catch (e: IOException) {
+                NotesUiState.Error
+            } catch (e: HttpException) {
+                NotesUiState.Error
+            }
         }
     }
 
